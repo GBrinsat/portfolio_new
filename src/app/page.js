746e835489b51a2e4715c12fcdf1305fe9b8bee7
Gerrit Brinsat <gerrit.brinsat@gmx.de>
2025-05-05 15:39:37 +0200

    Added ticker functionality to splash page. v.1.0

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import "./animation.css";
+
 export default function Home() {
   return (
     <div className="h-screen w-screen flex">
@@ -8,35 +10,62 @@ export default function Home() {
         <span className="text-8xl font-bold">Center Text</span>
         <div
           id="topScroller"
-          className="absolute -top-14 left-0 border-solid border-1 w-[902px] h-[40px] flex items-center"
+          className="absolute -top-14 left-0 w-[902px] h-[40px] flex items-center ticker-wrap"
         >
-          <span className="border-solid border-1 ml-3 mr-3 w-full">
-            testTop
-          </span>
+          <div className="ticker-top">
+            <span className="ticker__item">Wow!</span>
+            <span className="ticker__item">Incredible!</span>
+            <span className="ticker__item">
+              Want to know more? Click a link!
+            </span>
+          </div>
         </div>
+
         <div
           id="rightScroller"
-          className="absolute -right-14 top-0 border-solid border-1 h-[452px] w-[40px] flex justify-center"
+          className="absolute -right-14 top-0 h-[452px] w-[40px] flex flex-col justify-center items-center ticker-wrap-vertical"
         >
-          <span className="[writing-mode:vertical-lr] border-solid border-1 mt-3 mb-3">
-            testRight
-          </span>
+          <div className="ticker-right absolute top-full">
+            <div className="[writing-mode:vertical-lr] ticker__item_side">
+              WOW, is this a coincidence?
+            </div>
+            <div className="[writing-mode:vertical-lr] ticker__item_side">
+              Incredible if not
+            </div>
+            <div className="[writing-mode:vertical-lr] ticker__item_side">
+              So sexy, what happens if I write a lot more text though?!
+            </div>
+          </div>
         </div>
+
         <div
           id="bottomScroller"
-          className=" absolute -bottom-14 right-0 border-solid border-1 w-[902px] h-[40px] flex items-center"
+          className=" absolute -bottom-14 right-0 w-[902px] h-[40px] flex items-center ticker-wrap"
         >
-          <span className="border-solid border-1 ml-3 mr-3 w-full text-right">
-            testBottom
-          </span>
+          <div className="ticker-bottom">
+            <span className="ticker__item">Wow, such an amazing ticker</span>
+            <span className="ticker__item">Incredible FrontEnd skills</span>
+            <span className="ticker__item">
+              Want to know more? Click a link!
+            </span>
+          </div>
         </div>
+
         <div
           id="leftScroller"
-          className=" absolute -left-14 bottom-0 border-solid border-1 h-[452px] w-[40px] flex justify-center"
+          className=" absolute -left-14 bottom-0 h-[452px] w-[40px] flex justify-center ticker-wrap-vertical"
         >
-          <span className="[writing-mode:vertical-rl] border-solid border-1 mt-3 mb-3 text-right">
-            testLeft
-          </span>
+          <div className="ticker-left absolute">
+            <div className="[writing-mode:vertical-lr] ticker__item_side">
+              WOW, is this a coincidence?
+            </div>
+            <div className="[writing-mode:vertical-lr] ticker__item_side">
+              Incredible if not
+            </div>
+            <div className="[writing-mode:vertical-lr] ticker__item_side">
+              So sexy, what happens if I write a lot more text though?!
+            </div>
+          </div>
         </div>
       </div>
     </div>
